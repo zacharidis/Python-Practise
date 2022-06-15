1e75c39303ac951dba1f71d057aa10b4c3d42541
Georgios Zacharidis <zacharidis@icloud.com>
2022-06-15 03:01:34 +0300

    askisi4 added

--- askisi4.py
+++ askisi4.py
@@ -1,3 +1,24 @@
 #Python is fun 
 #i can see why people get obsessed with this language. 
 #askisi4 
+
+
+def stringModifier(word , charcounter):
+    wordlen = len(word);
+    
+    
+    
+    if(charcounter >= wordlen): 
+        return word
+    else: 
+         
+         mix = word[charcounter:]
+         print(mix)
+         return mix
+
+
+word = input("Please enter a string or a phrase or the story of your life:  ")
+counter = int(input("and now enter a positive integer :  "))
+print(stringModifier(word,counter))
+
+
