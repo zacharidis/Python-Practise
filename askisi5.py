0ce7b75d7e4ba29aada4b6150761c9a66a8fa218
Georgios Zacharidis <zacharidis@icloud.com>
2022-06-15 03:15:17 +0300

    askisi5 added

---
+++ askisi5.py
@@ -0,0 +1,18 @@
+from cmath import phase
+from urllib.request import ProxyHandler
+
+
+def checkTheIntegers(phrase):
+    firstElement = (phrase[0])
+    lastElement = (phrase[len(phrase)-1])
+    if (firstElement==lastElement):
+        return "ACCEPTED"
+    else:
+        return "NOT ACCEPTED"
+
+
+
+print("PLEASE ENTER A SERIES OF INTEGERS SEPPERATED BY AN EMPTY SPACE AND PRESS ENTER  :  ");
+phrase = input();
+print(checkTheIntegers(phrase));
+
