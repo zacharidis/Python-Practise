2fea9ef948e84b037205373aa5ef4a7003a97f7f
Georgios Zacharidis <zacharidis@icloud.com>
2022-06-16 23:10:00 +0300

    9

---
+++ askisi9.py
@@ -0,0 +1,10 @@
+numPropaidia = [1,2,3,4,5,6,7,8,9,10]
+
+for i in numPropaidia:
+    for j in numPropaidia:
+        print((i * j ))
+    print(" ")
+
+#kapos tha ginetai pio apla stin python SIGOURA ! episis pos kaneis print xoris allagi grammis
+
+    
